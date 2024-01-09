14f694e66eb7f8d47a16becf66facc041d584bd4
Shtepsiler <100515332+Shtepsiler@users.noreply.github.com>
2024-01-09 21:41:57 +0200

    comit

--- ServiceStation/MechanicPart/MechanicIdentiyServer/Program.cs
+++ ServiceStation/MechanicPart/MechanicIdentiyServer/Program.cs
@@ -1,4 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
+
+
+
+builder.Services.AddIdentityServer();
+
+builder.Services.AddIdentityServer()
+ .AddInMemoryClients(new List<Client>())
+ .AddInMemoryIdentityResources(new List<IdentityResource>())
+ .AddInMemoryApiResources(new List<ApiResource>())
+ .AddInMemoryApiScopes(new List<ApiScope>())
+ .AddTestUsers(new List<TestUser>())
+ .AddDeveloperSigningCredential();
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
